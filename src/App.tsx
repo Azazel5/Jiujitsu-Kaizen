c443a043f05d60d9fc4377178c201d66b6eb8f80
Azazel5 <subhanga2013@gmail.com>
2023-04-28 16:51:09 -0500

    Added clickHandler and states
    - to track if position or technique has been selected
    - using proper TS interfaces and types
    - starting to see some problems with TS as well; it is not all sunshine and rainbows!

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,61 @@
-import './App.css';
+import { useState } from 'react'
+import './App.scss';
 
 import Home from './pages/Home/Home';
 import ConciseCard from './components/Card/ConsiceCard/ConciseCard';
 
+interface MovePicker {
+    position: string | null,
+    technique: string | null
+}
+
 const App = () => {
+    const [selectedMove, setSelectedMove] = useState<MovePicker>({ position: "", technique: "" })
+
+    const conciseCardClickHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+        const input = e.target as HTMLElement;
+
+        if (!selectedMove.position) {
+            setSelectedMove({ position: input.innerText, technique: "" })
+        }
+
+        else {
+            setSelectedMove(move => ({ position: move.position, technique: input.innerText }))
+        }
+    }
+
+    // TODO: add this color on click with animation = #1a73e8
     return (
         <div className='mt-1 p-4'>
             <Home />
 
             <div className='columns'>
                 <div className='column'>
-                    <ConciseCard 
-                        imageSrc='https://cdn.evolve-mma.com/wp-content/uploads/2021/04/bjj-full-guard.jpg' 
-                        cardTitle='Full Guard'/>
+                    <ConciseCard
+                        imageSrc='https://cdn.evolve-mma.com/wp-content/uploads/2021/04/bjj-full-guard.jpg'
+                        cardTitle='Full Guard'
+                        cardClickHandler={conciseCardClickHandler} />
                 </div>
 
                 <div className='column'>
-                    <ConciseCard 
+                    <ConciseCard
                         imageSrc='https://cdn.evolve-mma.com/wp-content/uploads/2018/08/BJJ-Side-Control.jpg'
-                        cardTitle='Side Control'/>
+                        cardTitle='Side Control'
+                        cardClickHandler={conciseCardClickHandler} />
                 </div>
 
                 <div className='column'>
-                    <ConciseCard 
+                    <ConciseCard
                         imageSrc='https://cdn.evolve-vacation.com/wp-content/uploads/2018/04/brazilian-jiu-jitsu-mount.jpg'
-                        cardTitle='Mount'/>
+                        cardTitle='Mount'
+                        cardClickHandler={conciseCardClickHandler} />
                 </div>
 
                 <div className='column'>
-                    <ConciseCard 
+                    <ConciseCard
                         imageSrc='https://www.grapplearts.com/wp-content/uploads/2019/05/11-bjj-crucifix-armbar-1-legs-back-hips-forward.jpg'
-                        cardTitle='Crucifix'/>
+                        cardTitle='Crucifix'
+                        cardClickHandler={conciseCardClickHandler} />
                 </div>
             </div>
         </div>
--- src/components/Card/ConsiceCard/ConciseCard.tsx
+++ src/components/Card/ConsiceCard/ConciseCard.tsx
@@ -4,15 +4,16 @@ import { AiFillCheckCircle } from "react-icons/ai";
 
 interface Props {
     imageSrc: string,
-    cardTitle: string
+    cardTitle: string,
+    cardClickHandler: (e: React.MouseEvent<HTMLDivElement>) => void
 }
 
-const ConciseCard = ({ imageSrc, cardTitle }: Props): JSX.Element => {
+const ConciseCard = ({ imageSrc, cardTitle, cardClickHandler }: Props): JSX.Element => {
 
     return (
-        <div className="card">
+        <div className="card" onClick={(e) => cardClickHandler(e)}>
             <AiFillCheckCircle />
-            <img className='card-image' src={imageSrc} alt='BJJ Position'/>
+            <img className='card-image' src={imageSrc} alt='BJJ Position' />
             <div className="card-content has-text-centered">
                 <p className="subtitle is-4">{cardTitle}</p>
             </div>
